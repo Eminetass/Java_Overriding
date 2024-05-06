1dcc7e40815a9f56c6f3f602f721bff450f90ef2
Emine Tas <eminetas126@gmail.com>
2024-05-07 00:20:07 +0300

    çalışan işçi uygulaması

---
+++ EngineerManager.java
@@ -0,0 +1,11 @@
+public class EngineerManager extends EmployeManager{
+
+    @Override
+    // mühendis çalışanlarına ekstra olarak her zam sonunda +50TL eklenmesini istiyoruz.
+    public void makeRaise(double raiseValue){
+        super.employeMoney += raiseValue;
+        super.employeMoney += 50;
+        System.out.println("->Başarılı Bir Şekilde "+employeName+" Çalışanına "+raiseValue +" TL ve Mühendis Olduğu İçin" +
+                " +50 TL Zam Yapıldı.\n-> Yeni Maaş: "+super.employeMoney +"TL \n----------");
+    }
+}
