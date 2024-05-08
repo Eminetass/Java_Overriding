59e37ca804cf4f773e8f425f7e1945f7ba730354
Emine Tas <eminetas126@gmail.com>
2024-05-08 19:30:20 +0300

    çalışan işçi uygulaması

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+
+        EmployeManager employeManager = new EmployeManager();
+        employeManager.addEmploye("Metehan","Durmuş","Beden İşçisi",4000);
+        System.out.println(employeManager.getEmployeInfo());
+        employeManager.makeRaise(500);
+        EngineerManager engineerManager = new EngineerManager();
+        engineerManager.addEmploye("Oktay","Güden","Elektrik Elektronik Mühendisi",7500);
+        System.out.println(engineerManager.getEmployeInfo());
+        engineerManager.makeRaise(500);
+    }
+}
